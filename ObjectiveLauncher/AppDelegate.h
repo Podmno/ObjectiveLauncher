1b87e576f96daaf35c4d064e9ab27d7decb707fc
Podmno <podmno@icloud.com>
2024-01-06 16:37:14 +0800

    Initial Commit

---
+++ ObjectiveLauncher/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ObjectiveLauncher
+//
+//  Created by Ki MNO on 2024/1/6.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
